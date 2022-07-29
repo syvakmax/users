5f4f70b95a2382db66b156b31bedf73f2feea99b
syvakmax <syvakmax@gmail.com>
2022-07-29 17:30:02 +0300

    production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD3B9uub4pweuH-NqGxseO8qs5MHY-mSRI",
+    authDomain: "users-8b9a2.firebaseapp.com",
+    projectId: "users-8b9a2",
+    storageBucket: "users-8b9a2.appspot.com",
+    messagingSenderId: "787253609036",
+    appId: "1:787253609036:web:f264566f099fe2b56f6ae9"
+  }
 };
